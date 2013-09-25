7168f33c9355d7a9712128950b21685f6fc0dcf4
Per <perre_j@hotmail.com>
2013-09-24 21:32:19 -0400

    Configured for deployment to Heroku

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,9 @@ module ReviewSite
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password, :password_confirmation]
+    
+    # Heroku requires this to be false
+    config.assets.initialize_on_precompile=false
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
