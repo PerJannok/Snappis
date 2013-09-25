9c2d6b406d68758581b5fe20285fc26f75a6b467
Per <perre_j@hotmail.com>
2013-09-24 22:29:09 -0400

    Configured for deployment to Heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -235,7 +235,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
   
-  API_KEYS = YAML::load_file("#{Rails.root}/config/api_keys.yml")[Rails.env]
+  API_KEYS = YAML::load_file("#{Rails.root}/config/api_keys.yml")[Rails.env] if Rails.env.development?
   
   config.omniauth :google_oauth2, API_KEYS['google_api_key'], API_KEYS['google_api_secret'], { access_type: "offline", approval_prompt: "" }
   config.omniauth :twitter, API_KEYS['twitter_api_key'], API_KEYS['twitter_api_secret']
