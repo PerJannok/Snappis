959f0e4c6b4936a0451c2bd24bcfd56bd7a51407
Per <perre_j@hotmail.com>
2013-11-30 09:33:12 -0500

    new home view added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,21 @@
 class HomeController < ApplicationController
+
   def index
-    @users = User.all
+  	puts "home controller"
+  	@location = Location.new
+    #@location = Location.find(params[:id])
+    puts "got locations"
+
   end
+  
+  def show
+  	puts "show..."
+    @location = Location.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @location }
+    end
+  end
+
 end
